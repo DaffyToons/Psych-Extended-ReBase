b0085b6bdc6f55c8b080fe07c43778cc28f1c5b1
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2025-10-30 22:26:42 -0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -399,10 +399,7 @@ class Paths
 	}
 
 	inline static public function formatToSongPath(path:String) {
-		final invalidChars = ~/[~&;:<>#\s]/g;
-		final hideChars = ~/[.,'"%?!]/g;
-
-		return hideChars.replace(invalidChars.replace(path, '-'), '').trim().toLowerCase();
+		return path.toLowerCase().replace(' ', '-');
 	}
 
 	public static var currentTrackedSounds:Map<String, Sound> = [];

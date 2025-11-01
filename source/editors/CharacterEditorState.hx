a845e33eb5edf03771b9cc6b40b7efdabb407c4e
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2025-11-01 01:17:08 -0300

    Reverting the Editors

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -22,7 +22,7 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 {
 	var useDesktopThings:Bool = #if TOUCH_CONTROLS (ClientPrefs.data.KeyboardFixes ? true : false) #else true #end;
 	var character:Character;
-	var ghost:Character;
+	var ghost:FlxSprite;
 	var animateGhost:FlxAnimate;
 	var animateGhostImage:String;
 	var cameraFollowPointer:FlxSprite;
@@ -45,10 +45,8 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 	var _goToPlayState:Bool = true;
 
 	var anims = null;
-	var ghostAnims = null;
 	var animsTxt:FlxText;
 	var curAnim = 0;
-	var curGhostAnim = 0;
 
 	private var camEditor:FlxCamera;
 	private var camHUD:FlxCamera;
@@ -102,6 +100,11 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 		silhouettes.add(boyfriend);
 
 		silhouettes.alpha = 0.25;
+
+		ghost = new FlxSprite();
+		ghost.visible = false;
+		ghost.alpha = ghostAlpha;
+		add(ghost);
 		
 		animsTxt = new FlxText(10, 32, 400, '');
 		animsTxt.setFormat(null, 16, FlxColor.WHITE, LEFT, OUTLINE_FAST, FlxColor.BLACK);
@@ -190,7 +193,6 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 			"CHARACTER",
 			"A - Reset Current Offset",
 			"V/D - Previous/Next Animation",
-			"Q/E - Ghost's Previous/Next Animation",
 			"Arrow Buttons - Move Offset",
 			"",
 			"OTHER",
@@ -250,7 +252,6 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 	function addCharacter(reload:Bool = false)
 	{
 		var pos:Int = -1;
-
 		if(character != null)
 		{
 			pos = members.indexOf(character);
@@ -304,47 +305,62 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 
 		//var hideGhostButton:PsychUIButton = null;
 		var makeGhostButton:PsychUIButton = new PsychUIButton(25, 15, "Make Ghost", function() {
-			ghostAnims = anims.copy();
-			var anim = ghostAnims[curAnim];
+			var anim = anims[curAnim];
 			if(!character.isAnimationNull())
 			{
-				var pos = members.indexOf(character);
-				//Better Ghost
-				if(ghost != null)
+				var myAnim = anims[curAnim];
+				if(!character.isAnimateAtlas)
 				{
-					remove(ghost);
-					ghost.destroy();
+					ghost.loadGraphic(character.graphic);
+					ghost.frames.frames = character.frames.frames;
+					ghost.animation.copyFrom(character.animation);
+					ghost.animation.play(character.animation.curAnim.name, true, false, character.animation.curAnim.curFrame);
+					ghost.animation.pause();
 				}
-
-				var isPlayerGhost = character.isPlayer;
-				ghost = new Character(0, 0, _char, isPlayerGhost);
-				if(ghost.editorIsPlayer != null && isPlayerGhost != ghost.editorIsPlayer)
+				else if(myAnim != null) //This is VERY unoptimized and bad, I hope to find a better replacement that loads only a specific frame as bitmap in the future.
 				{
-					ghost.isPlayer = !ghost.isPlayer;
-					ghost.flipX = (ghost.originalFlipX != ghost.isPlayer);
+					if(animateGhost == null) //If I created the animateGhost on create() and you didn't load an atlas, it would crash the game on destroy, so we create it here
+					{
+						animateGhost = new FlxAnimate(ghost.x, ghost.y);
+						animateGhost.showPivot = false;
+						insert(members.indexOf(ghost), animateGhost);
+						animateGhost.active = false;
+					}
+
+					if(animateGhost == null || animateGhostImage != character.imageFile)
+						Paths.loadAnimateAtlas(animateGhost, character.imageFile);
+					
+					if(myAnim.indices != null && myAnim.indices.length > 0)
+						animateGhost.anim.addBySymbolIndices('anim', myAnim.name, myAnim.indices, 0, false);
+					else
+						animateGhost.anim.addBySymbol('anim', myAnim.name, 0, false);
+
+					animateGhost.anim.play('anim', true, false, character.atlas.anim.curFrame);
+					animateGhost.anim.pause();
+
+					animateGhostImage = character.imageFile;
 				}
-				ghost.debugMode = true;
-				ghost.missingCharacter = false;
+				
+				var spr:FlxSprite = !character.isAnimateAtlas ? ghost : animateGhost;
+				if(spr != null)
+				{
+					spr.setPosition(character.x, character.y);
+					spr.antialiasing = character.antialiasing;
+					spr.flipX = character.flipX;
+					spr.alpha = ghostAlpha;
 
-				add(ghost);
-				remove(character);
-				add(character);
+					spr.scale.set(character.scale.x, character.scale.y);
+					spr.updateHitbox();
 
-				// Copy character properties to ghost
-				ghost.flipX = character.flipX;
-				ghost.scale.set(character.scale.x, character.scale.y);
+					spr.offset.set(character.offset.x, character.offset.y);
+					spr.visible = true;
 
-				// Play the same animation
-				ghost.playAnim(character.getAnimationName(), true, false, character.animation.curAnim.curFrame);
-				ghost.animation.pause();
-				ghost.x = character.x;
-				ghost.y = character.y;
-				ghost.offset.x = anim.offsets[0];
-				ghost.offset.y = anim.offsets[1];
-
-				ghost.alpha = ghostAlpha;
-				ghost.visible = true;
-				//trace('created ghost character');
+					var otherSpr:FlxSprite = (spr == animateGhost) ? ghost : animateGhost;
+					if(otherSpr != null) otherSpr.visible = false;
+				}
+				/*hideGhostButton.active = true;
+				hideGhostButton.alpha = 1;*/
+				trace('created ghost image');
 			}
 		});
 
@@ -363,6 +379,12 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 			ghost.colorTransform.redOffset = value;
 			ghost.colorTransform.greenOffset = value;
 			ghost.colorTransform.blueOffset = value;
+			if(animateGhost != null)
+			{
+				animateGhost.colorTransform.redOffset = value;
+				animateGhost.colorTransform.greenOffset = value;
+				animateGhost.colorTransform.blueOffset = value;
+			}
 		};
 
 		var ghostAlphaSlider:PsychUISlider = new PsychUISlider(15, makeGhostButton.y + 25, function(v:Float)
@@ -908,22 +930,8 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 
 		if(lastZoom != FlxG.camera.zoom) cameraZoomText.text = 'Zoom: ' + FlxMath.roundDecimal(FlxG.camera.zoom, 2) + 'x';
 
-		/* CHARACTER/GHOST CONTROLS */
+		// CHARACTER CONTROLS
 		var changedAnim:Bool = false;
-		var changedGhostAnim:Bool = false;
-		if(ghostAnims != null && ghostAnims.length > 1)
-		{
-			if((/* FlxG.keys.justPressed.W */ #if TOUCH_CONTROLS mobilePad.buttonQ.justPressed #end) && (changedGhostAnim = true)) curGhostAnim--;
-			else if((/* FlxG.keys.justPressed.S */ #if TOUCH_CONTROLS mobilePad.buttonE.justPressed #end) && (changedGhostAnim = true)) curGhostAnim++;
-
-			if(changedGhostAnim)
-			{
-				curGhostAnim = FlxMath.wrap(curGhostAnim, 0, ghostAnims.length-1);
-				ghost.playAnim(ghostAnims[curGhostAnim].anim, true);
-				ghost.offset.x = ghostAnims[curGhostAnim].offsets[0];
-				ghost.offset.y = ghostAnims[curGhostAnim].offsets[1];
-			}
-		}
 		if(anims.length > 1)
 		{
 			if((FlxG.keys.justPressed.W #if TOUCH_CONTROLS || mobilePad.buttonV.justPressed #end) && (changedAnim = true)) curAnim--;
@@ -1155,6 +1163,9 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 		/////////////
 		// bg data //
 		/////////////
+		#if !BASE_GAME_FILES
+		camEditor.bgColor = 0xFF666666;
+		#else
 		var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
 		add(bg);
 
@@ -1162,6 +1173,7 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 		stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
 		stageFront.updateHitbox();
 		add(stageFront);
+		#end
 
 		dadPosition.set(100, 100);
 		bfPosition.set(770, 100);
@@ -1394,7 +1406,7 @@ class CharacterEditorState extends MusicBeatState implements PsychUIEventHandler
 	function onMouseEvent(e:MouseEvent):Void
 	{
 		#if TOUCH_CONTROLS
-		if (mobilePad != null)
+		if (mobilePad != null && !mobilePad.anyPressed([ANY]))
 			switch (e.type)
 			{
 				case MouseEvent.MOUSE_DOWN:
--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -208,7 +208,9 @@ class ChartingState extends MusicBeatState
 				player2: 'dad',
 				gfVersion: 'gf',
 				speed: 1,
+				offset: 1,
 				stage: 'stage',
+				format: 'psych_v1',
 				validScore: false
 			};
 			addSection();
@@ -397,7 +399,8 @@ class ChartingState extends MusicBeatState
 		updateGrid();
 
 		#if TOUCH_CONTROLS
-		if (usingMobile) addMobilePad("CHART_EDITOR", "CHART_EDITOR");
+		addMobilePad("CHART_EDITOR", "CHART_EDITOR");
+		if (!usingMobile) mobilePad.active = mobilePad.visible = false;
 		#end
 
 		super.create();
@@ -508,11 +511,16 @@ class ChartingState extends MusicBeatState
 		stepperSpeed.name = 'song_speed';
 		blockPressWhileTypingOnStepper.push(stepperSpeed);
 		#if MODS_ALLOWED
-		var directories:Array<String> = [Paths.mods('characters/'), Paths.mods(Mods.currentModDirectory + '/characters/'), Paths.getSharedPath('characters/')];
-		for(mod in Mods.getGlobalMods())
+		var directories:Array<String> = [
+			Paths.mods('characters/'), Paths.mods(Mods.currentModDirectory + '/characters/'), Paths.getSharedPath('characters/'),
+			Paths.mods('data/characters/'), Paths.mods(Mods.currentModDirectory + '/data/characters/'), Paths.getSharedPath('data/characters/')
+		];
+		for(mod in Mods.getGlobalMods()) {
 			directories.push(Paths.mods(mod + '/characters/'));
+			directories.push(Paths.mods(mod + '/data/characters/'));
+		}
 		#else
-		var directories:Array<String> = [Paths.getSharedPath('characters/')];
+		var directories:Array<String> = [Paths.getSharedPath('characters/'), Paths.getSharedPath('data/characters/')];
 		#end
 
 		var tempArray:Array<String> = [];
@@ -529,11 +537,14 @@ class ChartingState extends MusicBeatState
 			if(FileSystem.exists(directory)) {
 				for (file in Paths.readDirectory(directory)) {
 					var path = haxe.io.Path.join([directory, file]);
-					if (!FileSystem.isDirectory(path) && file.endsWith('.json')) {
-						var charToCheck:String = file.substr(0, file.length - 5);
-						if(charToCheck.trim().length > 0 && !charToCheck.endsWith('-dead') && !tempArray.contains(charToCheck)) {
-							tempArray.push(charToCheck);
-							characters.push(charToCheck);
+					var fileTypes:Array<String> = ['.json', '.xml']; //make this easier to change
+					for (fileType in fileTypes) {
+						if (!FileSystem.isDirectory(path) && file.endsWith(fileType)) {
+							var charToCheck:String = file.substr(0, file.length - fileType.length);
+							if(charToCheck.trim().length > 0 && !charToCheck.endsWith('-dead') && !tempArray.contains(charToCheck)) {
+								tempArray.push(charToCheck);
+								characters.push(charToCheck);
+							}
 						}
 					}
 				}
@@ -1495,6 +1506,11 @@ class ChartingState extends MusicBeatState
 	var playtestingOnComplete:Void->Void = null;
 	override function closeSubState()
 	{
+		persistentUpdate = true;
+		#if TOUCH_CONTROLS
+		addMobilePad("CHART_EDITOR", "CHART_EDITOR");
+		if (!usingMobile) mobilePad.active = mobilePad.visible = false;
+		#end
 		if(playtesting)
 		{
 			FlxG.sound.music.pause();
@@ -1524,9 +1540,6 @@ class ChartingState extends MusicBeatState
 			DiscordClient.changePresence("Chart Editor", StringTools.replace(_song.song, '-', ' '));
 			#end
 		}
-		#if TOUCH_CONTROLS
-		persistentUpdate = true;
-		#end
 		super.closeSubState();
 	}
 
@@ -1785,23 +1798,22 @@ class ChartingState extends MusicBeatState
 						{
 							if (touch.overlaps(note))
 							{
-								if (FlxG.keys.pressed.ALT)
+								#if TOUCH_CONTROLS
+								if (mobilePad.buttonF.pressed)
+								{
+									selectNote(note);
+								}
+								else #end if (FlxG.keys.pressed.ALT)
 								{
 									selectNote(note);
 									curSelectedNote[3] = curNoteTypes[currentType];
 									updateGrid();
 								}
-                                #if TOUCH_CONTROLS
-								else if (mobilePad.buttonF.pressed)
-								{
-									selectNote(note);
-								}
-								#end 
 								else
 								{
 									// trace('tryin to delete note...');
 									deleteNote(note);
-                                }
+								}
 							}
 						});
 					}
@@ -1933,10 +1945,9 @@ class ChartingState extends MusicBeatState
 				playtesting = true;
 				playtestingTime = Conductor.songPosition;
 				playtestingOnComplete = FlxG.sound.music.onComplete;
-				#if TOUCH_CONTROLS
 				persistentUpdate = false;
-				#end
 				openSubState(new editors.EditorPlayState(playbackSpeed));
+				#if TOUCH_CONTROLS removeMobilePad(); #end
 			}
 			else if (FlxG.keys.justPressed.ENTER #if TOUCH_CONTROLS || mobilePad.buttonA.justPressed #end)
 			{
@@ -1965,6 +1976,7 @@ class ChartingState extends MusicBeatState
 
 
 			if (FlxG.keys.justPressed.BACKSPACE #if TOUCH_CONTROLS || mobilePad.buttonB.justPressed #end) {
+				persistentUpdate = false; //disable the state update for fixing the quitting issue
 				// Protect against lost data when quickly leaving the chart editor.
 				autosaveSong();
 				PlayState.chartingMode = false;
@@ -2935,7 +2947,7 @@ class ChartingState extends MusicBeatState
 			note.noteType = i[3];
 		} else { //Event note
 			note.loadGraphic(Paths.image('eventArrow'));
-			note.rgbShader.enabled = false;
+			if (ClientPrefs.data.useRGB) note.rgbShader.enabled = false;
 			note.eventName = getEventName(i[1]);
 			note.eventLength = i[1].length;
 			if(i[1].length < 2)

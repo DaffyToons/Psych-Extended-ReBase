4d2e7652060e6b17e743567d7ef078708d6992f5
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2025-11-02 17:20:46 -0300

    FORGOT THIS

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2983,7 +2983,6 @@ class PlayState extends MusicBeatState
 
 				if (storyPlaylist.length <= 0)
 				{
-					Mods.loadTopMod();
 					FlxG.sound.playMusic(Paths.music('freakyMenu'));
 
 					cancelMusicFadeTween();
@@ -3038,7 +3037,6 @@ class PlayState extends MusicBeatState
 			else
 			{
 				trace('WENT BACK TO FREEPLAY??');
-				Mods.loadTopMod();
 				cancelMusicFadeTween();
 				if(FlxTransitionableState.skipNextTransIn) {
 					CustomFadeTransition.nextCamera = null;
